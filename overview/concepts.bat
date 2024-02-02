5a7ff096ca8c92946b7b04cc1ae5291cef2bfdac
hsimonis <helmut.simonis@insight-centre.org>
2024-02-02 08:27:18 +0000

    more papers, keyword analysis

---
+++ overview/concepts.bat
@@ -0,0 +1,24 @@
+pdfgrep -c -i "alternative constraint" papers/*.pdf
+pdfgrep -c -i "cumulative" papers/*.pdf
+pdfgrep -c -i "disjunctive" papers/*.pdf
+pdfgrep -c -i "diffn" papers/*.pdf
+pdfgrep -c -i "table constraint" papers/*.pdf
+pdfgrep -c -i "regular expression" papers/*.pdf
+pdfgrep -c -i "circuit" papers/*.pdf
+pdfgrep -c -i "nooverlap" papers/*.pdf
+pdfgrep -c -i "endbeforestart" papers/*.pdf
+pdfgrep -c -i "alwaysin" papers/*.pdf
+pdfgrep -c -i "span constraint" papers/*.pdf
+pdfgrep -c -i "bin.?packing" papers/*.pdf
+pdfgrep -c -i "cplex" papers/*.pdf
+pdfgrep -c -i "gurobi" papers/*.pdf
+pdfgrep -c -i "gecode" papers/*.pdf
+pdfgrep -c -i "choco" papers/*.pdf
+pdfgrep -c -i "mistral" papers/*.pdf
+pdfgrep -c -i "or.?tools" papers/*.pdf
+pdfgrep -c  "OPL" papers/*.pdf
+pdfgrep -c -i "cpo" papers/*.pdf
+pdfgrep -c -i "chuffed" papers/*.pdf
+pdfgrep -c -i "sicstus" papers/*.pdf
+pdfgrep -c -i "eclipse" papers/*.pdf
+pdfgrep -c -i "mini.?zinc" papers/*.pdf
