72f1e1dafc11b280efec149631dcf9f163eff347
hsimonis <helmut.simonis@insight-centre.org>
2024-02-06 19:41:59 +0000

    fixed some missing entries

--- overview/concepts.bat
+++ overview/concepts.bat
@@ -1,24 +1,30 @@
-pdfgrep -c -i "alternative constraint" papers/*.pdf
-pdfgrep -c -i "cumulative" papers/*.pdf
-pdfgrep -c -i "disjunctive" papers/*.pdf
-pdfgrep -c -i "diffn" papers/*.pdf
-pdfgrep -c -i "table constraint" papers/*.pdf
-pdfgrep -c -i "regular expression" papers/*.pdf
-pdfgrep -c -i "circuit" papers/*.pdf
-pdfgrep -c -i "nooverlap" papers/*.pdf
-pdfgrep -c -i "endbeforestart" papers/*.pdf
-pdfgrep -c -i "alwaysin" papers/*.pdf
-pdfgrep -c -i "span constraint" papers/*.pdf
-pdfgrep -c -i "bin.?packing" papers/*.pdf
-pdfgrep -c -i "cplex" papers/*.pdf
-pdfgrep -c -i "gurobi" papers/*.pdf
-pdfgrep -c -i "gecode" papers/*.pdf
-pdfgrep -c -i "choco" papers/*.pdf
-pdfgrep -c -i "mistral" papers/*.pdf
-pdfgrep -c -i "or.?tools" papers/*.pdf
-pdfgrep -c  "OPL" papers/*.pdf
-pdfgrep -c -i "cpo" papers/*.pdf
-pdfgrep -c -i "chuffed" papers/*.pdf
-pdfgrep -c -i "sicstus" papers/*.pdf
-pdfgrep -c -i "eclipse" papers/*.pdf
-pdfgrep -c -i "mini.?zinc" papers/*.pdf
+pdfgrep -c -i "alternative constraint" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "cumulative" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "disjunctive" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "diffn" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "table constraint" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "regular expression" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "circuit" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "nooverlap" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "endbeforestart" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "alwaysin" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "span constraint" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "bin.?packing" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "cplex" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "gurobi" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "gecode" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "choco" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "mistral" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "or.?tools" papers/*.pdf articles/*.pdf
+pdfgrep -c  "OPL" papers/*.pdf articles/*.pdf
+pdfgrep -c  "CHIP" papers/*.pdf articles/*.pdf
+pdfgrep -c  "Z3" papers/*.pdf articles/*.pdf
+pdfgrep -c  "OZ" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "claire" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "cpo" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "chuffed" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "sicstus" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "eclipse" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "ilog solver" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "ilog scheduler" papers/*.pdf articles/*.pdf
+pdfgrep -c -i "mini.?zinc" papers/*.pdf articles/*.pdf
